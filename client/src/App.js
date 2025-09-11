7703911794d34b42b7137ef9cc5e3e5708c05a8b
Anonymous-solver <anik.uits29@gmail.com>
2025-09-11 13:51:02 +0600

    both test passed in frontend + backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>React + Express Monorepo</h1>
+      <p>Hello from React!</p>
     </div>
   );
 }
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,33 @@
+// module.exports = {
+//   roots: ["<rootDir>/server", "<rootDir>/client"],
+//   // roots: ["<rootDir>/server"],
+//   testMatch: ["**/__tests__/**/*.test.js"],
+//   transform: {
+//     "^.+\\.[tj]sx?$": "babel-jest"
+//   },
+//   testEnvironment: "jsdom"
+// };
+
 module.exports = {
-  // roots: ["<rootDir>/server", "<rootDir>/client"],
-  roots: ["<rootDir>/server"],
+  roots: ["<rootDir>/server", "<rootDir>/client"],
   testMatch: ["**/__tests__/**/*.test.js"],
   transform: {
-    "^.+\\.[tj]sx?$": "babel-jest"
+    "^.+\\.[tj]sx?$": "babel-jest",
   },
-//   testEnvironment: "jsdom"
+  testEnvironment: "node", // default for server
+  projects: [
+    {
+      displayName: "server",
+      testMatch: ["<rootDir>/server/**/__tests__/**/*.test.js"],
+      testEnvironment: "node",
+      setupFiles: ["<rootDir>/jest.setup.js"],
+    },
+    {
+      displayName: "client",
+      testMatch: ["<rootDir>/client/**/__tests__/**/*.test.js"],
+      testEnvironment: "jsdom",
+      setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    },
+  ],
 };
+
